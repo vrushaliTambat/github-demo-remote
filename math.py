e273c3e3e3a958b84a4b3c7ed741231888493999
vrushaliTambat <60786641+vrushaliTambat@users.noreply.github.com>
2022-01-04 19:20:36 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,11 @@
+#Add implementation
+def add(x,y):
+    return x+y
+#Substract
+def substract(x,y):
+    return x-y     #on master branch
+#Multiplication
+def multiply(x,y):
+    return x*y      #on Bug456
+#Divide implementation
+def divide(x,y):
